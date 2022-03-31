c7d42a70bb8b710cea6794a74421d08ef7e8d3ce
Stefan Schweter <stefan@schweter.it>
2022-03-31 11:41:49 +0200

    experiments: add updated version of flair log parser

--- experiments/clef-hipe-2022/flair-log-parser.py
+++ experiments/clef-hipe-2022/flair-log-parser.py
@@ -20,14 +20,15 @@ for log_dir in log_dirs:
     if not training_log.exists():
         print(f"No training.log found in {log_dir}")
     
-    matches = re.match(".*(bs.*?)-wsFalse-(e.*?)-(lr.*?)-layers-1-crfFalse-(\d+)", str(log_dir))
+    matches = re.match(".*(bs.*?)-(ws.*?)-(e.*?)-(lr.*?)-layers-1-crfFalse-(\d+)", str(log_dir))
     
     batch_size = matches.group(1)
-    epochs = matches.group(2)
-    lr = matches.group(3)
-    seed = matches.group(4)
+    ws = matches.group(2)
+    epochs = matches.group(3)
+    lr = matches.group(4)
+    seed = matches.group(5)
     
-    result_identifier = f"{batch_size}-{epochs}-{lr}"
+    result_identifier = f"{ws}-{batch_size}-{epochs}-{lr}"
     
     with open(training_log, "rt") as f_p:
         all_dev_results = []
@@ -48,6 +49,8 @@ for log_dir in log_dirs:
                 
 mean_dev_results = {}
 
+print("Debug:", dev_results)
+
 for dev_result in dev_results.items():
     result_identifier, results = dev_result
     
@@ -57,9 +60,15 @@ for dev_result in dev_results.items():
 
 
 print("Averaged Development Results:")
-print(mean_dev_results)
+
+sorted_mean_dev_results = dict(sorted(mean_dev_results.items(), key=lambda item: item[1], reverse=True))
+
+for mean_dev_config, score in sorted_mean_dev_results.items():
+    print(f"{mean_dev_config} : {round(score * 100, 2)}")
 
 best_dev_configuration = max(mean_dev_results, key=mean_dev_results.get)
+
+print("")
 
 print("Best configuration:", best_dev_configuration)
 
@@ -67,3 +76,4 @@ print("")
 
 print("Best Development Score:",
       round(mean_dev_results[best_dev_configuration] * 100, 2))
+
