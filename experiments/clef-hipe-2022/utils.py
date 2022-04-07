ce5a241a1df332983cc156ab1c4048c65f30d6b5
Stefan Schweter <stefan@schweter.it>
2022-04-07 12:46:46 +0200

    experiments: add preprocessing function for NewsEye dataset (de-hyphenation support)

--- experiments/clef-hipe-2022/utils.py
+++ experiments/clef-hipe-2022/utils.py
@@ -115,10 +115,110 @@ def prepare_clef_2020_corpus(
         for line in lines:
             f_out.write(line + "\n")
 
+def prepare_newseye_corpus(
+    file_in: Path, file_out: Path, eos_marker: str, document_separator: str, add_document_separator: bool
+):
+    with open(file_in, "rt") as f_p:
+        original_lines = f_p.readlines()
+
+    lines = []
+
+    # Add missing newline after header
+    lines.append(original_lines[0])
+
+    for line in original_lines[1:]:
+        if line.startswith(" \t"):
+            # Workaround for empty tokens
+            continue
+
+        line = line.strip()
+
+        # Add "real" document marker
+        if add_document_separator and line.startswith(document_separator):
+            lines.append("-DOCSTART- O")
+            lines.append("")
+
+        lines.append(line)
+
+        if eos_marker in line:
+            lines.append("")
+
+    # Now here comes the de-hyphenation part ;)
+    word_seperator = "¬"
+    
+    for index, line in enumerate(lines):
+        if line.startswith("#"):
+            continue
+
+        if not line:
+            continue
+
+        last_line = lines[index - 1]
+        last_line_splitted = last_line.split("\t")
+
+        if not last_line_splitted[0].endswith(word_seperator):
+            continue
+    
+        # The following example
+        #
+        # den	O	O	O	null	null	SpaceAfter
+        # Ver¬	B-LOC	O	O	null	n                  <- last_line
+        # einigten	I-LOC	O	O	null	n	SpaceAfter <- current_line
+        # Staaten	I-LOC	O	O	null	n
+        # .	O	O	O	null	null
+        #
+        # will be transformed to:
+        #
+        # den	O	O	O	null	null	SpaceAfter
+        # Vereinigten	B-LOC	O	O	null	n	|Normalized-8
+        # #einigten	I-LOC	O	O	null	n	SpaceAfter|Commented
+        # Staaten	I-LOC	O	O	null	n
+        # .	O	O	O	null	null
+    
+        suffix = last_line.split("\t")[0].replace(word_seperator, "")  # Will be "Ver" 
+
+        prefix_length = len(line.split("\t")[0])
+
+        # Override last_line:
+        # Ver¬ will be transformed to Vereinigten with normalized information at the end
+
+        last_line_splitted[0] = suffix + line.split("\t")[0]
+
+        last_line_splitted[9] += f"|Dehyphenated-{prefix_length}"
+
+        current_line_splitted = line.split("\t")
+        current_line_splitted[0] = "#" + current_line_splitted[0]
+        current_line_splitted[-1] += "|Commented"
+
+        lines[index - 1] = "\t".join(last_line_splitted)
+        lines[index]     = "\t".join(current_line_splitted)
+
+    # Post-Processing I
+    # Beautify: _|Commented –> Commented
+    for index, line in enumerate(lines):
+        if not line:
+            continue
+        
+        if not line.startswith("#"):
+            continue
+        
+        current_line_splitted = line.split("\t")
+    
+        if current_line_splitted[-1] == "_|Commented":
+            current_line_splitted[-1] = "Commented"
+            lines[index] = "\t".join(current_line_splitted)
+        
+    # Finally, save it!
+    with open(file_out, "wt") as f_out:
+        for line in lines:
+            f_out.write(line + "\n")
+
+
 if __name__ == "__main__":
     # Please make sure, that you've cleaned the dataset folder when using own preprocessing function
     from flair.datasets import NER_HIPE_2022
 
+    # HIPE-2020
     hipe_2020_de = NER_HIPE_2022(dataset_name="hipe2020",
                                  language="de",
                                  version="v2.0",
@@ -135,6 +235,18 @@ if __name__ == "__main__":
                                  add_document_separator=True,
                                  preproc_fn=prepare_clef_2020_corpus)
 
+    # NewsEye - de-hyphenation is currently only useful for German and French
+    newseye_de = NER_HIPE_2022(dataset_name="newseye",
+                               language="de",
+                               version="v2.0",
+                               add_document_separator=True,
+                               preproc_fn=prepare_newseye_corpus)
+    newseye_fr = NER_HIPE_2022(dataset_name="newseye",
+                               language="fr",
+                               version="v2.0",
+                               add_document_separator=True,
+                               preproc_fn=prepare_newseye_corpus)
+
     # Number of sentences and documents should never change when doing de-hyphenation!
     # Check that here:
     assert len(hipe_2020_de.train) == 3470 + 2 + 103
@@ -142,3 +254,8 @@ if __name__ == "__main__":
     assert len(hipe_2020_en.dev) == 1045 + 80
     assert len(hipe_2020_fr.train) == 5743 + 158
     assert len(hipe_2020_fr.dev) == 1244 + 43
+
+    assert len(newseye_de.train) == 20839 + 1 + 7
+    assert len(newseye_de.dev) == 1110 + 1 + 12
+    assert len(newseye_fr.train) == 7106 + 1 + 35
+    assert len(newseye_fr.dev) == 662 + 1 + 35
