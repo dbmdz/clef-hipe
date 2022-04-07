d5628b4575b8edce386b463994105782ec21a908
Stefan Schweter <stefan@schweter.it>
2022-04-07 14:27:47 +0200

    experiments: add new de-hyphenation preprocessing for Finnish and Swedish NewsEye dataset

--- experiments/clef-hipe-2022/utils.py
+++ experiments/clef-hipe-2022/utils.py
@@ -213,6 +213,133 @@ def prepare_newseye_corpus(
         for line in lines:
             f_out.write(line + "\n")
 
+def prepare_newseye_fi_sv_corpus(
+    file_in: Path, file_out: Path, eos_marker: str, document_separator: str, add_document_separator: bool
+):
+    with open(file_in, "rt") as f_p:
+        original_lines = f_p.readlines()
+
+    lines = []
+
+    # Add missing newline after header
+    lines.append(original_lines[0])
+
+    for line in original_lines[1:]:
+        if line.startswith(" \t"):
+            # Workaround for empty tokens
+            continue
+
+        line = line.strip()
+
+        # Add "real" document marker
+        if add_document_separator and line.startswith(document_separator):
+            lines.append("-DOCSTART- O")
+            lines.append("")
+
+        lines.append(line)
+
+        if eos_marker in line:
+            lines.append("")
+
+    # Now here comes the de-hyphenation part
+    # And we want to avoid matching "-DOCSTART-" lines here, so append a tab
+    word_seperator = "-\t"
+
+    for index, line in enumerate(lines):
+        if line.startswith("#"):
+            continue
+
+        if line.startswith(word_seperator):
+            continue
+
+        if not line:
+            continue
+
+        prev_line = lines[index - 1]
+
+        prev_prev_line = lines[index - 2]
+
+        if not prev_line.startswith(word_seperator):
+            continue
+
+        # Example:
+        # Po  NoSpaceAfter <- prev_prev_line
+        # -   <- prev_line
+        # len <- current_line
+        #
+        # will be de-hyphenated to:
+        #
+        # Polen Dehyphenated-3
+        # # -
+        # # len
+        #
+        # It is really important, that "NoSpaceAfter" in the previous
+        # line before hyphenation character! Otherwise, it is no real
+        # hyphenation!
+
+        if not "NoSpaceAfter" in prev_line:
+            continue
+
+        if not prev_prev_line:
+            continue
+
+        suffix = line.split("\t")[0]
+
+        prev_prev_line_splitted = lines[index - 2].split("\t")
+        prev_prev_line_splitted[0] += suffix
+
+        prev_line_splitted = lines[index - 1].split("\t")
+        prev_line_splitted[0] = "#" + prev_line_splitted[0]
+        prev_line_splitted[-1] += "|Commented"
+
+        current_line_splitted = line.split("\t")
+        current_line_splitted[0] = "#" + current_line_splitted[0]
+        current_line_splitted[-1] += "|Commented"
+
+        # Add some meta information about suffix length
+        # Later, it is possible to re-construct original token and suffix
+        prev_prev_line_splitted[9] += f"|Dehyphenated-{len(suffix)}"
+
+        lines[index - 2] = "\t".join(prev_prev_line_splitted)
+        lines[index - 1] = "\t".join(prev_line_splitted)
+        lines[index]     = "\t".join(current_line_splitted)
+
+    # Post-Processing I
+    for index, line in enumerate(lines):
+        if not line:
+            continue
+
+        if not line.startswith(word_seperator):
+            continue
+
+        # oh noooo
+        current_line_splitted = line.split("\t")
+        current_line_splitted[0] = "#" + current_line_splitted[0]
+
+        current_line_splitted[-1] += "|Commented"
+
+        lines[index] = "\t".join(current_line_splitted)
+
+    # Post-Processing II
+    # Beautify: _|Commented –> Commented
+    for index, line in enumerate(lines):
+        if not line:
+            continue
+
+        if not line.startswith("#"):
+            continue
+
+        current_line_splitted = line.split("\t")
+
+        if current_line_splitted[-1] == "_|Commented":
+            current_line_splitted[-1] = "Commented"
+            lines[index] = "\t".join(current_line_splitted)
+
+    # Finally, save it!
+    with open(file_out, "wt") as f_out:
+        for line in lines:
+            f_out.write(line + "\n")
+
 
 if __name__ == "__main__":
     # Please make sure, that you've cleaned the dataset folder when using own preprocessing function
@@ -235,7 +362,7 @@ if __name__ == "__main__":
                                  add_document_separator=True,
                                  preproc_fn=prepare_clef_2020_corpus)
 
-    # NewsEye - de-hyphenation is currently only useful for German and French
+    # NewsEye - German and French
     newseye_de = NER_HIPE_2022(dataset_name="newseye",
                                language="de",
                                version="v2.0",
@@ -246,6 +373,18 @@ if __name__ == "__main__":
                                version="v2.0",
                                add_document_separator=True,
                                preproc_fn=prepare_newseye_corpus)
+
+    # NewsEye - Swedish and Finnish uses another hyphenation format
+    newseye_fi = NER_HIPE_2022(dataset_name="newseye",
+                               language="fi",
+                               version="v2.0",
+                               add_document_separator=True,
+                               preproc_fn=prepare_newseye_fi_sv_corpus)
+    newseye_sv = NER_HIPE_2022(dataset_name="newseye",
+                               language="sv",
+                               version="v2.0",
+                               add_document_separator=True,
+                               preproc_fn=prepare_newseye_fi_sv_corpus)
 
     # Number of sentences and documents should never change when doing de-hyphenation!
     # Check that here:
@@ -259,3 +398,8 @@ if __name__ == "__main__":
     assert len(newseye_de.dev) == 1110 + 1 + 12
     assert len(newseye_fr.train) == 7106 + 1 + 35
     assert len(newseye_fr.dev) == 662 + 1 + 35
+
+    assert len(newseye_fi.train) == 1141 + 1 + 24
+    assert len(newseye_fi.dev) == 140 + 1 + 24
+    assert len(newseye_sv.train) == 1063 + 1 + 21
+    assert len(newseye_sv.dev) == 126 + 1 + 21
